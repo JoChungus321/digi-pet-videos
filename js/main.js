09f9ab5c8f39bdbb9181422a700ca2428afc3a8e
Joseph Fernandez <47188070+JoChungus321@users.noreply.github.com>
2019-04-05 18:32:19 -0400

    Imported html, css, js, and mp4 folders

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('swap').addEventListener('click', function(event) {
+  document.getElementById('player').currentSrc= 'videos\\2.mp4';
+    // document.getElementById('player').innerHTML= '';
+    console.log(document.getElementById('player').innerHTML);
+})
